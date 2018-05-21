bb8552fc9f944475dd3dc9932bd0a4c538add22e
Jeet Sukumaran <jeetsukumaran@gmail.com>
2018-05-21 14:25:39 -0400

    Update history search terms

--- fztricks.bash
+++ fztricks.bash
@@ -58,7 +58,21 @@ function fzh() {
     else
         SEDEX='s/^ *\([0-9]*\)\** *//'
     fi
-    LINE=$(HISTTIMEFORMAT= history | sed "$SEDEX" | uniq | fz --stdout --output-relative-paths -)
+    # 1. Clear history format to get history in the form of '[number] [command]'
+    # 2. Apply `sed` to strip leading number
+    # 3. Apply `sed` to strip leading spaces
+    # 4. Apply `sed` to strip trailing spaces
+    # 5. Reverse list so that most recent commands are seen first ...
+    # 6. Filter for duplicate lines, retaining order, keeping the first copy of each dupe (and because we reversed the list, this will the most recent command)
+    # 7. Reverse list again that most recent commands are seen last (and thus appear at the top of the fuzzysnake list) ...
+    # 8. Pass to fuzzysnake
+    LINE=$(HISTTIMEFORMAT= history \
+            | sed "$SEDEX" \
+            | sed -e 's/^[ \t]*//' \
+            | sed -e 's/[ \t]*$//' \
+            | awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' \
+            | awk '{ if (!h[$0]) { print $0; h[$0]=1 } }' \
+            | fz --stdout --output-relative-paths -)
     echo $LINE
 }
 
